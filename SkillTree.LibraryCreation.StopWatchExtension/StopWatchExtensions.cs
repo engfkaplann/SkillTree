b8ea3fde8a45c2a57e96e74ba33bd3b19b4a21c3
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-16 19:26:03 +0300

    LibraryCreation example added

---
+++ SkillTree.LibraryCreation.StopWatchExtension/StopWatchExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.LibraryCreation.StopWatchExtension
+{
+    public static class StopWatchExtensions
+    {
+        public static double CalculateExecutionTime(this Stopwatch stopwatch, Func<object> function)
+        {
+            stopwatch.Start();
+            function();
+            stopwatch.Stop();
+            TimeSpan duration = stopwatch.Elapsed;
+
+            stopwatch.Reset();
+
+            return duration.TotalMilliseconds;
+        }
+    }
+}
