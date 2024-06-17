d848cf2ae58bd237d1f7d69976ef40c261d3d1c2
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-17 23:02:21 +0300

    Prototype example added

---
+++ SkillTree.Prototype/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.Prototype.Models
+{
+    public class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ SkillTree.Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿using SkillTree.Prototype.Models;
+using System;
+
+namespace SkillTree.Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", 26);
+
+            Person clonePerson = (Person)person.Clone();
+
+            Console.WriteLine($"Name: {clonePerson.Name}");
+            Console.WriteLine($"Age: {clonePerson.Age}");
+
+            Console.ReadLine();
+        }
+    }
+}
