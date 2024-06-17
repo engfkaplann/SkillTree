e15d7c9cb86cd8d302d28e4a4a17bb751534cabd
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-17 22:04:22 +0300

    Singleton example added

---
+++ SkillTree.Singleton/Models/Concretes/SingletonClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.Singleton.Models.Concretes
+{
+    public class SingletonClass
+    {
+        private static SingletonClass _instance;
+        private static readonly object _lock = new object();
+
+        public static SingletonClass Instance
+        {
+            get 
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        _instance = new SingletonClass();
+                    }
+
+                }
+
+                return _instance;
+            }
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something");
+        }
+    }
+}
---
+++ SkillTree.Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using SkillTree.Singleton.Models.Concretes;
+using System;
+
+namespace SkillTree.Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonClass singletonObject = SingletonClass.Instance;
+
+            singletonObject.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+}
