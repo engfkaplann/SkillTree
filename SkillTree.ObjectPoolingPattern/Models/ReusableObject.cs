6adebcfbfded26de9b8faecb9463bc92322ccbe2
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-18 07:44:11 +0300

    ObjectPoolingPattern example added

---
+++ SkillTree.ObjectPoolingPattern/Models/ReusableObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.ObjectPoolingPattern.Models
+{
+    public class ReusableObject
+    {
+        private static int _idCounter = 1;
+
+        private int _id;
+
+        public ReusableObject()
+        {
+            _id = _idCounter;
+            
+            Console.WriteLine($"{nameof(ReusableObject)}object created. Id : {_id}");
+
+            _idCounter++;
+        }
+
+        public void Write() => Console.WriteLine($"Id : {_id}");
+        
+    }
+}
---
+++ SkillTree.ObjectPoolingPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using SkillTree.ObjectPoolingPattern.Models;
+using SkillTree.ObjectPoolingPattern.Models.Concretes;
+using System;
+
+namespace SkillTree.ObjectPoolingPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectPool<ReusableObject> objectPool = new ObjectPool<ReusableObject>(() => new ReusableObject());
+
+            ReusableObject object1 = objectPool.Get();
+            object1.Write();
+            objectPool.Return(object1);
+
+            ReusableObject object2 = objectPool.Get();
+            object2.Write();
+
+            ReusableObject object3 = objectPool.Get();
+            object3.Write();
+
+            objectPool.Return(object2);
+
+            Console.ReadLine();
+        }
+    }
+}
