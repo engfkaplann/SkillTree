d234c8883ff947ae25f4c36ff258c27d5e4c969d
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-18 04:48:32 +0300

    VersionedRepositoryPattern example added

---
+++ SkillTree.VersionedRepositoryPattern/Models/Concretes/RepositoryBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillTree.VersionedRepositoryPattern.Models.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.VersionedRepositoryPattern.Models.Concretes
+{
+    public class RepositoryBase<TEntity> : IRepository<TEntity> where TEntity : EntityBase
+    {
+        private readonly DbContext _context;
+
+        public RepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public void Add(TEntity entity)
+        {
+            entity.VersionNumber = 1;
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            var existingEntity = _context.Set<TEntity>().Find(entity.Id);
+            if (existingEntity != null)
+            {
+                existingEntity.VersionNumber++;
+                _context.Entry(entity).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+        }
+
+        public void Remove(int id)
+        {
+            TEntity entity = _context.Set<TEntity>().Find(id);
+            if (entity != null)
+            {
+                _context.Set<TEntity>().Remove(entity);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SkillTree.VersionedRepositoryPattern/Models/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using SkillTree.VersionedRepositoryPattern.Models.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.VersionedRepositoryPattern.Models.Entities
+{
+    public class Product : EntityBase
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
