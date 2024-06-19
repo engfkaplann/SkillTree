62ef499a2f102f157e718789176538720d885a46
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-19 04:17:03 +0300

    Serialization example added

---
+++ SkillTree.Serialization/Models/Entities/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.Serialization.Models.Entities
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public bool Graduated { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(int age, string name, bool graduated)
+        {
+            Age = age;
+            Name = name;
+            Graduated = graduated;
+        }
+    }
+}
---
+++ SkillTree.Serialization/Program.cs
@@ -0,0 +1,36 @@
+﻿using SkillTree.Serialization.Models.Entities;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace SkillTree.Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(21, "John", true);
+
+            string xml = SerializeToXml(person);
+            string json = SerializeToJson(person);
+
+            Console.ReadLine();
+        }
+
+        private static string SerializeToXml(Person person)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            using (StringWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, person);
+                return writer.ToString();
+            }
+        }
+
+        private static string SerializeToJson(Person person)
+        {
+            return JsonSerializer.Serialize(person);
+        }
+    }
+}
