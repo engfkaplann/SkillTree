507086b5ed91cb8056dc240f1b478af4badd6041
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-19 04:49:23 +0300

    Reflection example added

---
+++ SkillTree.Reflection/Models/Entities/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.Reflection.Models.Entities
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public bool Graduated { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(int age, string name, bool graduated)
+        {
+            Age = age;
+            Name = name;
+            Graduated = graduated;
+        }
+
+        public void SayMyName()
+        {
+            Console.WriteLine($"My name is {Name}");
+        }
+    }
+}
---
+++ SkillTree.Reflection/Program.cs
@@ -0,0 +1,61 @@
+﻿using SkillTree.Reflection.Models.Entities;
+using System;
+using System.Reflection;
+
+namespace SkillTree.Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(22, "John", true);
+
+            TypeNameWriter(person);
+            CallMethod(person, "SayMyName");
+            PropertyWriter(person, "Age");
+
+            Console.ReadLine();
+        }
+
+        private static void TypeNameWriter(object obj)
+        {
+            var className = obj.GetType().Name;
+            Console.WriteLine($"Type Name: {className}");
+        }
+
+        private static void CallMethod(object obj, string methodName)
+        {
+            try
+            {
+                Type type = obj.GetType();
+                MethodInfo method = type.GetMethod(methodName);
+                method.Invoke(obj, null);
+            }
+            catch
+            {
+                Console.WriteLine($"Method {methodName} not found");
+            }
+        }
+
+        private static void PropertyWriter(object obj, string propertyName)
+        {
+            try
+            {
+                Type type = obj.GetType();
+                PropertyInfo propertyInfo = type.GetProperty(propertyName);
+
+                object propertyValue = propertyInfo.GetValue(obj);
+
+                if (propertyValue != null)
+                {
+                    Console.WriteLine($"Property {propertyName} Value: {propertyValue}");
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Propert {propertyName} not found");
+            }
+            
+        }
+    }
+}
