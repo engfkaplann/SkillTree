38c40bc05eb5cca0e5e669713c74a69f42b68d4c
Furkan Kaplan <furkan.kaplan@keove.com>
2024-06-19 05:27:45 +0300

    MemoryCache example added

---
+++ SkillTree.MemoryCache/Models/Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTree.MemoryCache.Models.Entities
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public bool Graduated { get; set; }
+
+        public Person(int age, string name, bool graduated)
+        {
+            Age = age;
+            Name = name;
+            Graduated = graduated;
+        }
+    }
+}
---
+++ SkillTree.MemoryCache/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using SkillTree.MemoryCache.Models.Entities;
+using System;
+
+namespace SkillTree.MemoryCache
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Extensions.Caching.Memory.MemoryCache cache = new Microsoft.Extensions.Caching.Memory.MemoryCache(new MemoryCacheOptions());
+
+            Person person = new Person(25, "John", true);
+
+            cache.Set("key", person, TimeSpan.FromSeconds(30));
+
+            if (cache.TryGetValue("key", out Person cachedValue))
+            {
+                Console.WriteLine($"Cached object name is {cachedValue.Name}");
+            }
+
+            cache.Remove("key");
+
+            if (cache.TryGetValue("key", out Person removed))
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine($"Object not found");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
